b503cff8b0d9c8930d1319eac841cd3d3b9d491a
Rick <fyfy441298@163.com>
2020-07-19 18:17:58 +0800

    fx

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ from nameko.rpc import rpc
 class GreetingService:
     name = "greeting_service" # 自定义服务名称
 
-    @rpc #入口点标记
+    @rpc #入口点标记1
     def hello(self, name):
         return "hello. %s??".format(name)
